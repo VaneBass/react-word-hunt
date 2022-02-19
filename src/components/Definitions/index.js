7d0b429211ddbf8cf12a953944dc67555c452b9d
VaneBass <1412109759@qq.com>
2022-02-19 12:37:02 +0800

    fixed some bugs

--- src/components/Definitions/index.js
+++ src/components/Definitions/index.js
@@ -7,11 +7,9 @@ const Definitions = ({ word, hasMeaning, meanings, category, lightMode }) => {
       {/* 当语言是英文时，显示音频 */}
       {meanings[0] && word && category === "en" && (
         <audio
-          src={
-            meanings[0].phonetics[0] &&
-            `https:` + meanings[0].phonetics[0].audio
-          }
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
           style={{
+            width: "100%",
             display: "block",
             backgroundColor: "#fff",
             borderRadius: 10,
@@ -44,7 +42,7 @@ const Definitions = ({ word, hasMeaning, meanings, category, lightMode }) => {
                     {def.example}
                   </span>
                 )}
-                {def.synonyms.length !== 0 && (
+                {def.synonyms && def.synonyms.length !== 0 && (
                   <span>
                     <b>Synonyms: </b>
                     {def.synonyms.map((s, index, arr) => {
